f47e48f7bf51241f9f5930d1adc11d987aa22414
KatoTheFirst <164291082+KatoTheFirst@users.noreply.github.com>
2024-05-09 10:22:41 +0700

    Create Class.cpp

---
+++ LTNC-06/Class.cpp
@@ -0,0 +1,63 @@
+#include <iostream>
+#include <sstream>
+using namespace std;
+
+/*
+Enter code for class Student here.
+Read statement for specification.
+*/
+class Student {
+    private:
+        int age, standard;
+        string first_name, last_name, res = "";
+    public:
+        void set_age(int a) {
+            age = a;
+        }
+        void set_standard(int b) {
+            standard = b;
+        }
+        void set_first_name(string c) {
+            first_name = c;
+        }
+        void set_last_name(string d) {
+            last_name = d;
+        }
+        int get_age() {
+            return age;
+        }
+        int get_standard() {
+            return standard;
+        }
+        string get_first_name() {
+            return first_name;
+        }
+        string get_last_name() {
+            return last_name;
+        }
+        string to_string() {
+            res = std::to_string(age) + ',' + first_name + ',' + last_name + ',' + std::to_string(standard);
+            return res;
+        }
+};
+
+int main() {
+    int age, standard;
+    string first_name, last_name;
+    
+    cin >> age >> first_name >> last_name >> standard;
+    
+    Student st;
+    st.set_age(age);
+    st.set_standard(standard);
+    st.set_first_name(first_name);
+    st.set_last_name(last_name);
+    
+    cout << st.get_age() << "\n";
+    cout << st.get_last_name() << ", " << st.get_first_name() << "\n";
+    cout << st.get_standard() << "\n";
+    cout << "\n";
+    cout << st.to_string();
+    
+    return 0;
+}
